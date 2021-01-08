fc082782bcbe5dda6a569da76ad480d2174d0cd8
ph1 <phild@01ab.wiki>
2021-01-08 08:03:32 +0300

    building libbuf

--- 08_TestingCoverage/src/buf.c
+++ 08_TestingCoverage/src/buf.c
@@ -23,16 +23,8 @@
  *         printf("values[%zu] = %f\n", i, values[i]);
  *     buf_free(values);
  */
-#include <stddef.h>
-#include <stdlib.h>
 
-#ifndef BUF_INIT_CAPACITY
-#  define BUF_INIT_CAPACITY 8
-#endif
-
-#ifndef BUF_ABORT
-#  define BUF_ABORT abort()
-#endif
+#include "buf.h"
 
 struct buf {
     size_t capacity;
@@ -40,48 +32,7 @@ struct buf {
     char buffer[];
 };
 
-#define buf_ptr(v) \
-    ((struct buf *)((char *)(v) - offsetof(struct buf, buffer)))
-
-#define buf_free(v) \
-    do { \
-        if (v) { \
-            free(buf_ptr((v))); \
-            (v) = 0; \
-        } \
-    } while (0)
-
-#define buf_size(v) \
-    ((v) ? buf_ptr((v))->size : 0)
-
-#define buf_capacity(v) \
-    ((v) ? buf_ptr((v))->capacity : 0)
-
-#define buf_push(v, e) \
-    do { \
-        if (buf_capacity((v)) == buf_size((v))) { \
-            (v) = buf_grow1(v, sizeof(*(v)), \
-                            !buf_capacity((v)) ? \
-                              BUF_INIT_CAPACITY : \
-                              buf_capacity((v))); \
-        } \
-        (v)[buf_ptr((v))->size++] = (e); \
-    } while (0)
-
-#define buf_pop(v) \
-    ((v)[--buf_ptr(v)->size])
-
-#define buf_grow(v, n) \
-    ((v) = buf_grow1((v), sizeof(*(v)), n))
-
-#define buf_trunc(v, n) \
-    ((v) = buf_grow1((v), sizeof(*(v)), n - buf_capacity(v)))
-
-#define buf_clear(v) \
-    ((v) ? (buf_ptr((v))->size = 0) : 0)
-
-
-static void *
+void *
 buf_grow1(void *v, size_t esize, ptrdiff_t n)
 {
     struct buf *p;
